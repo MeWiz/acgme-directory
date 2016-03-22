c64bf73193f457ff9da956e3047381dc0d3ecb23
Nikhil Goyal <subs@spandan.com>
2016-03-22 01:48:07 -0400

    First working version - now need to delete old rows when use table second time

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/funcs.js"></script>
+<title>ACGME Data Extractor</title>
+</head>
+
+<body>
+<div id="state_spl_div">
 <?php
-# Use the Curl extension to query Google and get back a page of results
+// get the master table of state names and specialty codes from the ACGME website
 $url = "https://apps.acgme.org/ads/Public/Programs/Search";
 $ch = curl_init();
 $timeout = 5;
@@ -10,12 +21,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false) or die (curl_error($ch));
 $html = curl_exec($ch) or die (curl_error($ch));
 curl_close($ch);
 
-# Create a DOM parser object
 $dom = new DOMDocument();
-
-# Parse the HTML from Google.
-# The @ before the method call suppresses any warnings that
-# loadHTML might throw because of invalid HTML in the page.
 @$dom->loadHTML($html);
 
 $states=$specialty=array();
@@ -31,17 +37,22 @@ foreach($select_spl->getElementsByTagName('option') as $opt) {
 	if (is_numeric($id)) $specialty[$id]=trim($opt->nodeValue);
 }
 ?>
-<html>
-<head>
-</head>
-<body>
-<form action="q_proglist.php" method="post">
+<form>
 <select id="state" name="state">
 <?php foreach ($states as $key=>$val) echo "\t<option value='$key'>$val</option>\r\n"; ?>
 </select>
 <select id="specialty" name="specialty">
 <?php foreach ($specialty as $key=>$val) echo "\t<option value='$key'>$val</option>\r\n"; ?>
 </select>
-<input type="submit" name="submit" id="submit" value="Submit">
+<input type="button" name="doit" id="doit" value="Search &gt;&gt;">
 </form>
+</div>
+<div id="progs">
+<table id="progtable">
+<thead>
+<tr><th>Program ID</th><th>Program Name</th><th>emails</th></tr>
+</thead>
+</table>
+</div>
 </body>
+</html>
---
+++ js/funcs.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+$(document).ready(function() {
+	"use strict";
+    $('#doit').on('click',function(){getdata();});
+});
+
+function getdata() {
+	"use strict";
+	$.ajax({
+		url: 'q_proglist.php',
+		type: 'GET',
+		data: {
+			state: $('#state').val(),
+			specialty: $('#specialty').val()
+		},
+		dataType: "json",
+		success: function(ret) {
+			$.each(ret, function(key, val) {
+				// create new row for each program
+				$('#progtable').append("<tr id='t_"+val.progid+"'><td>"+val.code+"</td><td>"+val.name+"</td></tr>");
+				// query email addresses for this program
+				$.ajax({
+					url: 'q_proginfo.php',
+					type: 'GET',
+					data: {
+						progid: val.progid
+					},
+					dataType:"json",
+					success: function(proginfo) {
+						var row_id="#t_"+proginfo.progid;	// id for row to which cells added
+						var email_str=proginfo.email.join(", ");
+						$(row_id).append("<td>"+email_str+"</td>");
+					},
+					error: function(jqXHR) {
+						console.log(jqXHR.responseText);
+					}
+				});
+			});
+		},
+		error: function (jqXHR) {
+			console.log(jqXHR.responseText);
+		}
+	});
+}
